e410de26100bd0d9a8fcb9bce2e2f6e695333f84
acharleseon <acharleseon@users.noreply.github.com>
2026-04-24 21:30:49 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-Pq3pZJQL.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Admin/Desktop/kindred-heart-engine-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/Dopemint2/assets/index-BKHdh9jI.js"] }, "/": { filePath: "C:/Users/Admin/Desktop/kindred-heart-engine-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/Dopemint2/assets/index-jf1qnKQB.js"] } }, clientEntry: "/Dopemint2/assets/index-BKHdh9jI.js" });
-export {
-  tsrStartManifest
-};
